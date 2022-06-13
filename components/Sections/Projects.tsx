c57e0d8daefdff094a8e321a11a4b580d494009b
jadennns <jadennns@yahoo.com>
2022-06-13 17:59:18 +0700

    Update

--- components/Sections/Projects.tsx
+++ components/Sections/Projects.tsx
@@ -25,31 +25,33 @@ const projects = [
 
 export default function Projects() {
   return (
-    <div className="flex flex-col space-y-4" id="projects">
-      <h1 className="text-white text-3xl font-bold">Projects</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        {projects.map((project, index) => (
-          <Link href={project.url} key={index + 1}>
-            <div className="w-[23rem] lg:w-[23rem] h-[16rem] bg-main-4 rounded-md flex flex-col space-y-2 cursor-pointer trans-grow">
-              <Image
-                src={project.image}
-                alt={project.name}
-                width={200}
-                height={150}
-                className="rounded-t-md"
-              />
-              <div className="px-2">
-                <p className="text-xl font-bold text-gray-200">
-                  {project.name}
-                </p>
-                <p className="text-sm text-main-1 font-semibold">
-                  {project.description}
-                </p>
+    <section id="projects">
+      <div className="flex flex-col space-y-4">
+        <h1 className="text-white text-3xl font-bold">Projects</h1>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+          {projects.map((project, index) => (
+            <Link href={project.url} key={index + 1}>
+              <div className="w-[23rem] lg:w-[23rem] h-[16rem] bg-main-4 rounded-md flex flex-col space-y-2 cursor-pointer trans-grow">
+                <Image
+                  src={project.image}
+                  alt={project.name}
+                  width={200}
+                  height={150}
+                  className="rounded-t-md"
+                />
+                <div className="px-2">
+                  <p className="text-xl font-bold text-gray-200">
+                    {project.name}
+                  </p>
+                  <p className="text-sm text-main-1 font-semibold">
+                    {project.description}
+                  </p>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
